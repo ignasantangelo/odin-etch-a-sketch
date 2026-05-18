2da9ba3224274ed77b4a314033dff1f366234cab
Ignacio <ignaciosantangelo25@gmail.com>
2026-05-18 21:02:47 +0000

    Add ability to change the size of the grid

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.getElementById('container');
 
-for (let i = 1; i <= 16; i++) {
+const size = 100;
+
+for (let i = 1; i <= size; i++) {
     let rowDiv = document.createElement('div');
     rowDiv.className = 'row';
 
-    for (let j = 1; j <= 16; j++) {
+    for (let j = 1; j <= size; j++) {
         let colDiv = document.createElement('div');
         colDiv.className = 'col';
 
